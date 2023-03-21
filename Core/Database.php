1a1b2d34179a38af8e18a7a94ba710d7d6dbe1cb
Alexotieno1717 <alexotieno900@gmail.com>
2023-03-21 11:09:52 +0300

    Name Spacing

--- Core/Database.php
+++ Core/Database.php
@@ -1,5 +1,8 @@
 <?php
 
+namespace Core;
+
+use PDO;
 
 // Connect to MySQL database
 class Database{
--- Core/router.php
+++ Core/router.php
@@ -1,11 +1,11 @@
 <?php
 
-$routes = require('routes.php');
+$routes = require base_path('routes.php');
 
 
 function routeToController($uri, $routes) {
     if (array_key_exists($uri, $routes)) {
-        require $routes[$uri];
+        require base_path($routes[$uri]);
     } else {
         abort();
     }
@@ -14,7 +14,7 @@ function routeToController($uri, $routes) {
 function abort($code = 404) {
     http_response_code($code);
 
-    require "views/{$code}.php";
+    require base_path("views/{$code}.php");
 
     die();
 }
