2fe5f2a50a7ae68cdc443c0156295189f5755929
Alexotieno1717 <alexotieno900@gmail.com>
2023-04-04 09:08:31 +0300

    Redirect user to login when not in sessions | when authenticated cannot access register page

--- Core/Middleware/Guest.php
+++ Core/Middleware/Guest.php
@@ -3,5 +3,11 @@
 namespace Core\Middleware;
 class Guest
 {
-
+    public function handle()
+    {
+        if (isset($_SESSION['user']) && $_SESSION['user']) {
+            header('Location: /');
+            exit();
+        }
+    }
 }
