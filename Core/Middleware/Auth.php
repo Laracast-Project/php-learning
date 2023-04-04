b7fbb63485b0bad5e99d6084ef10041d5824af47
Alexotieno1717 <alexotieno900@gmail.com>
2023-04-04 09:06:55 +0300

    Created Auth file | restricted user to create a post only when authenticated

--- Core/Middleware/Auth.php
+++ Core/Middleware/Auth.php
@@ -3,5 +3,11 @@
 namespace Core\Middleware;
 class Auth
 {
-
+    public function handle()
+    {
+        if (!isset($_SESSION['user'])) {
+            header('Location: /');
+            exit();
+        }
+    }
 }
---
+++ Core/Middleware/Middleware.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Core\Middleware;
+
+class Middleware
+{
+
+}
