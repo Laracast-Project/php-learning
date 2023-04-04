f0fbf1db4fce632b4d15a37c51442cb9b4aee0aa
Alexotieno1717 <alexotieno900@gmail.com>
2023-04-04 09:09:31 +0300

    Added Middleware class

--- Core/Middleware/Middleware.php
+++ Core/Middleware/Middleware.php
@@ -4,5 +4,23 @@ namespace Core\Middleware;
 
 class Middleware
 {
+    const MAP = [
+        'guest' => Guest::class,
+        'auth' => Auth::class
+    ];
 
+    public static function resolve($key)
+    {
+        if (!$key){
+            return;
+        }
+
+        $middleware = static::MAP[$key] ?: false;
+
+        if(! $middleware){
+            throw new \Exception('No Matching middleware found for the key.' . $key);
+        }
+
+        (new $middleware)->handle();
+    }
 }
